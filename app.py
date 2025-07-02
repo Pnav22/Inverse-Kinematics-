01039ae2835361ea156f9afb5cd00c4af5db85a4
Pranav Mukkara <mukkarapranav77@gmail.com>
2025-07-02 14:24:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ st.set_page_config(page_title="IK Simulator", layout="wide")
 st.title("Inverse Kinematics Simulator")
 
 # Embed the HTML/JS app here
-st.components.v1.html(html_code, height=700, scrolling=True)
+st.components.v1.html(html_code, height=700, scrolling=True
